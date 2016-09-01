d76512270410cf155f32202f4a709bc4d1eb07f1
Cal Lee <cal.christopheralee@gmail.com>
2016-08-31 20:06:55 -0400

    Updated ggplot settings and additional cluster analysis mainly

--- Classification.R
+++ Classification.R
@@ -16,13 +16,18 @@ E$cluster <- kmeans(Scaled, 6)$cluster
 library(ggmap)
 mapWorld<-borders("world", colour="gray50", fill="gray50")
 map<- ggplot(E) + mapWorld 
-mapPoints <- map + geom_point(data=E, aes(x = long, y = lat, colour=factor(cluster),alpha=0.8)) + theme(legend.position='right') + ggtitle('Cities Clustering')
+mapPoints <- map + geom_point(data=E, aes(x = long, y = lat, colour=factor(cluster)),alpha=0.8) + theme(legend.position='right') + ggtitle('Cities Clustering')
 mapPoints
 
-E$cluster2<- kmeans(Scaled[, c(2, 3, 4, 12, 13, 15, 18, 19)], 6)$cluster
-map + geom_point(data=E, aes(x = long, y = lat, colour=factor(cluster2)),alpha=0.7, size=0.5) + theme(legend.position='right') + ggtitle('Cities Clustering') + scale_radius(range=c(.4,1), guide=F)
+E$cluster2<- kmeans(Scaled[, c(2, 3, 4, 12, 13, 15, 18, 19)], 5)$cluster
+map + geom_point(data=E, aes(x = long, y = lat, colour=factor(cluster2)),alpha=0.7, size=0.8) + theme(legend.position='right') + ggtitle('Cities Clustering') + scale_radius(range=c(.4,1), guide=F)+ scale_color_discrete("Cluster")
 E$cluster3<-kmeans(Scaled[, c(2, 3, 4, 7, 12, 13, 15, 18, 19)], 2)$cluster
 map + geom_point(data=E, aes(x = long, y = lat, colour=factor(cluster3)),alpha=0.2, size=1) + theme(legend.position='right') + ggtitle('Cities Clustering') + scale_radius(range=c(.4,1), guide=F)
+E$cluster4<-kmeans(Scaled[, c(2, 3, 4, 7, 9, 13, 14, 15, 17, 18, 19, 20)], 3)$cluster
+map + geom_point(data=E, aes(x = long, y = lat, colour=factor(cluster4)),alpha=0.8, size=1) + theme(legend.position='right') + ggtitle('Cities Clustering') + scale_radius(range=c(.4,1), guide=F) + scale_color_discrete("Cluster")
+
+E$cluster5<-kmeans(Scaled[, c(2, 3, 4, 7, 14, 19, 20)], 4)$cluster
+map + geom_point(data=E, aes(x = long, y = lat, colour=factor(cluster5)),alpha=0.8, size=1) + theme(legend.position='right') + ggtitle('Cities Clustering') + scale_radius(range=c(.4,1), guide=F) + scale_color_discrete("Cluster")
 
 pc<-prcomp(Scaled)
 E<-cbind(E, pc=pc$x[,1:2])
