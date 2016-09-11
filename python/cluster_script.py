f9658285e5fc37cedab3ce5de5f1f9fe13d91921
Ivan Peng <ivan.peng@smartersoftware.co.nz>
2016-09-11 12:50:22 -0400

    Adding basic clustering functionality. Yet to add graphing

--- python/cluster_script.py
+++ python/cluster_script.py
@@ -1,4 +1,23 @@
+from database_setup import Session, FlatStat
+from sqlalchemy import create_engine
 
+from sklearn.cluster import DBSCAN
+import numpy as np
+
+
+def transform_to_np_array(query):
+    cinema_arr = []
+    educated_arr = []
+    for row in query:
+        # Have a flat stat instance. Transform to array here.
+        # As a placeholder, we will use the values of cinema as x axis, and uneducated as y
+        # Probably wrong though. Need to discuss what's the general process here.
+        if row.stat_id == 1:
+            cinema_arr.append(row.value)
+        else:
+            educated_arr.append(row.value*100)
+
+    return np.array(list(zip(cinema_arr, educated_arr)))
 
 def main():
     """
@@ -7,7 +26,19 @@ def main():
     What format do we need as output is the question. And how do we cluster on multiple dimensions?
     Also worth setting up the pipeline to graphing it.
     """
-    pass
+    # First grab data from db
+    engine = create_engine("sqlite:///cities.db")
+    session = Session(bind=engine)
+    # Grab all data from flat stats table
+    query = session.query(FlatStat).all()
+
+    X = transform_to_np_array(query)
+
+    # Run dbscan now
+    results = DBSCAN(eps=2, min_samples=10).fit(X)
+
+    # TODO: get matplotlib in here, but for now, just print all properties of dbscan
+    print(results.__dict__)
 
 
 if __name__ == "__main__":
