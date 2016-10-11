c5ba8fc549e592b788409b009e97b033e5c46376
Cal Lee <cal.christopheralee@gmail.com>
2016-10-10 22:52:14 -0400

    Algorithm for evaluating cluster outputs

---
+++ Calgorithm.R
@@ -0,0 +1,27 @@
+ #function to calculate the fit of a cluster output against a factor result
+ # n<-120
+ # x= as.factor(sample(c('a', 'b', 'c', 'd'), n, replace=T))
+ # y= sample(1:4, n, replace=T)
+ # library(combinat)
+ # scores=numeric(fact(4))
+
+ # for (i in 1:fact(4)){
+ 	# levels(x) <- permn(4)[[i]]
+ 	# scores[i] <- length(which(x==y))
+ # }
+ 
+calgorithm <- function(x, y) {
+ 	if (length(x) != length(y)) {
+ 		stop("Vector lengths do not match")
+ 	}
+ 	n <- length(y)
+ 	v <- length(unique(y))
+ 	scores=numeric(fact(v))
+ 	y <- as.numeric(y)
+ 	x <- as.factor(x)
+ 	for (i in 1:fact(v)){
+ 		levels(x) <- permn(v)[[i]]
+ 		scores[i] <- length(which(x==y))
+ 	}
+ 	return(max(scores)/n)
+ }
